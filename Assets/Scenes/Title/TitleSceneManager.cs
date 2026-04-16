37e7efd89aeb6d7538bc3a2ae77536758295a984
AlteNielsen <alte.nielsen@gmail.com>
2026-04-16 20:33:55 +0900

    作成：タイトル画面初期化処理

--- Assets/Scenes/Title/TitleSceneManager.cs
+++ Assets/Scenes/Title/TitleSceneManager.cs
@@ -21,6 +21,7 @@ public class TitleSceneManager : MonoBehaviour
         TitleSceneController();
 
         sceneView = new TitleSceneView(document);
+        SaveDataInitialize();
     }
 
     private void TitleSceneController()
@@ -31,6 +32,17 @@ public class TitleSceneManager : MonoBehaviour
         {
             buttons[i].clicked += methods[i];
         }
+    }
+
+    private void SaveDataInitialize()
+    {
+        SaveDataManager.Instance.Initialize((int)SaveDataManager.SaveDataChunk.MapSelect);
+        SaveDataManager.Instance.Initialize((int)SaveDataManager.SaveDataChunk.IndicatorSelect);
+        SaveDataManager.Instance.Initialize((int)SaveDataManager.SaveDataChunk.UnitSelect);
+        SaveDataManager.Instance.Initialize((int)SaveDataManager.SaveDataChunk.UnitLevel);
+        SaveDataManager.Instance.Initialize((int)SaveDataManager.SaveDataChunk.FireMap);
+        SaveDataManager.Instance.Initialize((int)SaveDataManager.SaveDataChunk.UnitMap);
+        SaveDataManager.Instance.Initialize((int)SaveDataManager.SaveDataChunk.UnitFacing);
     }
 
     public void OnClicked(int index)
