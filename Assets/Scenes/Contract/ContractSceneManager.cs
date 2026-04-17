2b5d65e82fe46166f2ba5c31a90319cadb347650
AlteNielsen <alte.nielsen@gmail.com>
2026-04-17 17:30:32 +0900

    作成：契約内容ボタンのクリック検知と見た目変更

--- Assets/Scenes/Contract/ContractSceneManager.cs
+++ Assets/Scenes/Contract/ContractSceneManager.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.UIElements;
 
 public class ContractSceneManager : MonoBehaviour
 {
     [SerializeField] private UIDocument document;
     private ContractSceneView sceneView;
+    private List<Button> indicatorButtons;
 
     void Awake()
     {
         sceneView = new ContractSceneView(document);
+        ContractSceneController();
+        for(int i = 0; i < 9; i++)
+        {
+            IndicatorButtonClicked(i, 0);
+        }
+    }
+
+    private void ContractSceneController()
+    {
+        indicatorButtons = document.rootVisualElement.Query<Button>("IndicatorButton").ToList();
+        int maxLv = 10;
+        for(int i = 0; i < indicatorButtons.Count; i++)
+        {
+            int j = i;
+            indicatorButtons[i].clicked += () => IndicatorButtonClicked(j / maxLv, j % maxLv);
+        }
+    }
+
+    private void IndicatorButtonClicked(int indicator, int lv)
+    {
+        sceneView.IndicatorButtonChange(indicator, lv);
     }
 }
