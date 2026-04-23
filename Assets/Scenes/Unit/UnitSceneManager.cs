efd7cbdc76819858d04230076d502f5fd9acd5f4
AlteNielsen <alte.nielsen@gmail.com>
2026-04-23 10:22:21 +0900

    追加：ユニットレベル変更機能

--- Assets/Scenes/Unit/UnitSceneManager.cs
+++ Assets/Scenes/Unit/UnitSceneManager.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public class UnitSceneManager : MonoBehaviour
 {
     [SerializeField] private UIDocument document;
     [SerializeField] private VisualTreeAsset unitCard;
     private UnitSceneView sceneView;
+    private int[] unitLevels;
 
     void Awake()
     {
         ExtinguishingContract.DevelopOnlyGameSetup();
         ScrollViewSetup();
         sceneView = new UnitSceneView(document);
+        unitLevels = new int[UnitDataBase.Datas.Length];
+        UnitSceneController();
+        for(int i = 0; i < UnitDataBase.Datas.Length; i++)
+        {
+            UnitLevelSelect(i, 0);
+        }
     }
 
     private void ScrollViewSetup()
@@ -23,5 +31,23 @@ public class UnitSceneManager : MonoBehaviour
             VisualElement card = unitCard.Instantiate();
             unitScrollView.contentContainer.Add(card);
         }
+    }
+
+    private void UnitSceneController()
+    {
+        List<Button> levelSelectors = document.rootVisualElement.Query<Button>("LevelSelector").ToList();
+        int maxLv = levelSelectors.Count / UnitDataBase.Datas.Length;
+        for(int i = 0; i < levelSelectors.Count; i++)
+        {
+            int index = i / maxLv;
+            int level = i % maxLv;
+            levelSelectors[i].clicked += () => UnitLevelSelect(index, level);
+        }
+    }
+
+    private void UnitLevelSelect(int index, int level)
+    {
+        unitLevels[index] = level;
+        sceneView.UnitLevelChange(index, level);
     }
 }
