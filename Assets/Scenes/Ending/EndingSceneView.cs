96f1cd15da1c44250f016e9511f9142d46aca568
AlteNielsen <alte.nielsen@gmail.com>
2026-04-28 00:44:21 +0900

    追加：ファイルの作成とスクリーンの文字表示、結果による切り替え、統計画面への遷移

---
+++ Assets/Scenes/Ending/EndingSceneView.cs
@@ -0,0 +1,81 @@
+using UnityEngine.UIElements;
+using System.Collections.Generic;
+using System;
+
+public class EndingSceneView
+{
+    private UIDocument mainDocument;
+    private UIDocument goodDocument;
+    private UIDocument badDocument;
+
+    public EndingSceneView(UIDocument doc, UIDocument good, UIDocument bad)
+    {
+        mainDocument = doc;
+        goodDocument = good;
+        badDocument = bad;
+        WriteTexts();
+        DisplayEndingScreen();
+    }
+
+    private void WriteTexts()
+    {
+        WriteGoodTexts();
+        WriteBadTexts();
+        WriteMainTexts();
+    }
+
+    private void WriteGoodTexts()
+    {
+        List<Label> labels = goodDocument.rootVisualElement.Query<Label>("Text").ToList();
+        for(int i = 0; i < labels.Count; i++)
+        {
+            labels[i].text = TextDataBase.GetTexts(TextDataBase.TextDictionary.EndingScreen)[i];
+        }
+    }
+
+    private void WriteBadTexts()
+    {
+        List<Label> labels = badDocument.rootVisualElement.Query<Label>("Text").ToList();
+        for (int i = 0; i < labels.Count; i++)
+        {
+            labels[i].text = TextDataBase.GetTexts(TextDataBase.TextDictionary.EndingScreen)[3 + i];
+        }
+    }
+
+    private void WriteMainTexts()
+    {
+
+    }
+
+    private void DisplayEndingScreen()
+    {
+        ReadOnlySpan<float> blockSituation = SaveDataManager.Instance.Access<BurningSituationChunk>((int)SaveDataManager.SaveDataChunk.BurningSituation).data.Span;
+        bool isGoodEnding = true;
+        for(int i = 0; i < blockSituation.Length; i++)
+        {
+            if(blockSituation[i] > 0.5f)
+            {
+                isGoodEnding = false;
+                break;
+            }
+        }
+
+        if(isGoodEnding)
+        {
+            goodDocument.sortingOrder = 10;
+            badDocument.sortingOrder = -10;
+        }
+        else
+        {
+            goodDocument.sortingOrder = -10;
+            badDocument.sortingOrder = 10;
+        }
+    }
+
+    public void SwitchMainScreen()
+    {
+        goodDocument.sortingOrder = -10;
+        badDocument.sortingOrder = -11;
+        mainDocument.sortingOrder = 10;
+    }
+}
