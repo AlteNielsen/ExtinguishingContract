8cfb1b5bd857fc791106214f6ca698db015fe629
AlteNielsen <alte.nielsen@gmail.com>
2026-04-28 17:04:06 +0900

    追加：MaxIDへの書き込みと全初期化処理

--- Assets/Scenes/Ending/EndingSceneManager.cs
+++ Assets/Scenes/Ending/EndingSceneManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -13,6 +14,8 @@ public class EndingSceneManager : MonoBehaviour
         ExtinguishingContract.DevelopOnlyGameSetup();
         sceneView = new EndingSceneView(mainDocument, goodDocument, badDocument);
         EndingSceneController();
+        UpdateMaxID();
+        SaveDataManager.Instance.EndingSceneSaveDataInitialize();
     }
 
     private void EndingSceneController()
@@ -21,5 +24,17 @@ public class EndingSceneManager : MonoBehaviour
         goodNext.clicked += sceneView.SwitchMainScreen;
         Button badNext = badDocument.rootVisualElement.Q<Button>("NextButton");
         badNext.clicked += sceneView.SwitchMainScreen;
+    }
+
+    private void UpdateMaxID()
+    {
+        ReadOnlySpan<float> now = SaveDataManager.Instance.Access<NowIDChunk>((int)SaveDataManager.SaveDataChunk.NowID).data.Span;
+        int nowGrade = CulculateLibrary.ContractGrade(now);
+        ReadOnlySpan<float> max = SaveDataManager.Instance.Access<MaxIDChunk>((int)SaveDataManager.SaveDataChunk.MaxID).data.Span;
+        int maxGrade = CulculateLibrary.ContractGrade(max);
+        if(nowGrade >= maxGrade)
+        {
+            SaveDataManager.Instance.SetData((int)SaveDataManager.SaveDataChunk.MaxID, now.ToArray());
+        }
     }
 }
