c9a69058d9f38d91b55e9c57ac7c5f86893b51a2
AlteNielsen <alte.nielsen@gmail.com>
2026-04-30 12:14:38 +0900

    リファクタ：盤面の描画を独立

--- Assets/Scenes/Stage/StageSceneManager.cs
+++ Assets/Scenes/Stage/StageSceneManager.cs
@@ -3,26 +3,15 @@ using UnityEngine.UIElements;
 
 public class StageSceneManager : MonoBehaviour
 {
-    private const int tileNum = 96;
     [SerializeField] private UIDocument document;
     [SerializeField] private VisualTreeAsset tile;
     private StageSceneView sceneView; 
+    private StageBoardView boardView;
 
     void Awake()
     {
         ExtinguishingContract.DevelopOnlyGameSetup();
-        BoardSetup();
+        boardView = new StageBoardView(document, tile);
         sceneView = new StageSceneView(document);
-    }
-
-    private void BoardSetup()
-    {
-        VisualElement board = document.rootVisualElement.Q<VisualElement>("Board");
-        board.contentContainer.Clear();
-        for(int i = 0; i < tileNum; i++)
-        {
-            VisualElement ve = tile.Instantiate();
-            board.contentContainer.Add(ve);
-        }
     }
 }
