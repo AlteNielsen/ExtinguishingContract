b0d4e0683be8a3a67ed601c753c6a7327f6bd2a1
AlteNielsen <alte.nielsen@gmail.com>
2026-05-02 12:54:53 +0900

    リファクタ：計算クラスの別ファイル退避とコンストラクタ改良

---
+++ Assets/Scenes/Stage/StageCalculators.cs
@@ -0,0 +1,239 @@
+using System;
+
+public class FireCalculator
+{
+    private bool[] mapLayout;
+    private int width;
+    private int height;
+
+    public FireCalculator(Span<bool> map, int wid, int hei)
+    {
+        mapLayout = new bool[wid * hei];
+        map.CopyTo(mapLayout);
+        width = wid;
+        height = hei;
+    }
+
+    public void FireSpread(Span<bool> result, Span<bool> original, Span<bool> water)
+    {
+        FireSpreadUnit(result, original);
+        BlockFireByMap(result);
+        BlockFireByWater(result, water);
+    }
+
+    private void FireSpreadUnit(Span<bool> result, Span<bool> original)
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (original[i])
+            {
+                int posX = i % width;
+                int posY = i / width;
+                if (0 < posX)
+                {
+                    result[i - 1] = true;
+                }
+                if ((posX + 1) < width)
+                {
+                    result[i + 1] = true;
+                }
+                if (0 < posY)
+                {
+                    result[i - width] = true;
+                }
+                if ((posY + 1) < height)
+                {
+                    result[i + width] = true;
+                }
+            }
+        }
+    }
+
+    private void BlockFireByMap(Span<bool> result)
+    {
+        for (int i = 0; i < result.Length; ++i)
+        {
+            if (mapLayout[i])
+            {
+                result[i] = false;
+            }
+        }
+    }
+
+    private void BlockFireByWater(Span<bool> result, Span<bool> water)
+    {
+        for (int i = 0; i < result.Length; ++i)
+        {
+            if (water[i])
+            {
+                result[i] = false;
+            }
+        }
+    }
+}
+
+public class WaterCalculator
+{
+    private bool[] mapLayout;
+    private int width;
+    private int height;
+
+    public WaterCalculator(Span<bool> map, int wid, int hei)
+    {
+        mapLayout = new bool[wid * hei];
+        map.CopyTo(mapLayout);
+        width = wid;
+        height = hei;
+    }
+
+    public void WaterCalculate(Span<bool> result, Span<int> unitMap, Span<UnitFacing> unitFacing)
+    {
+        ReadOnlySpan<float> levels = SaveDataManager.Instance.Access<UnitLevelChunk>((int)SaveDataManager.SaveDataChunk.UnitLevel).data.Span;
+        for (int i = 0; i < unitMap.Length; i++)
+        {
+            if (unitMap[i] >= 0)
+            {
+                UnitWaterCalc(result, i % width, i / width, unitMap[i], (int)levels[unitMap[i]], unitFacing[unitMap[i]]);
+
+            }
+        }
+    }
+
+    private void UnitWaterCalc(Span<bool> result, int unitPosX, int unitPosY, int unitID, int unitLevel, UnitFacing facing)
+    {
+        RangeData[] range = UnitDataBase.Datas[unitID].RangeData;
+        for (int i = 0; i < unitLevel; i++)
+        {
+            for (int j = 0; j < range[i].range.Length; j++)
+            {
+                var (relTargetX, relTargetY) = RotatePos(range[i].range[j].relativeX, range[i].range[j].relativeY, facing);
+                bool isValid = !CheckIsBlocked(unitPosX, unitPosY, unitPosX + relTargetX, unitPosY + relTargetY);
+                if (isValid)
+                {
+                    result[(unitPosY + relTargetY) * width + unitPosX + relTargetX] = true;
+                }
+            }
+        }
+    }
+
+    private (int relRangePosX, int relRangePosY) RotatePos(int relPosX, int relPosY, UnitFacing facing)
+    {
+        switch (facing)
+        {
+            case UnitFacing.North:
+                return (relPosX, relPosY);
+            case UnitFacing.East:
+                return (relPosY, -relPosX);
+            case UnitFacing.South:
+                return (-relPosX, -relPosY);
+            case UnitFacing.West:
+                return (-relPosY, relPosX);
+        }
+        return (relPosX, relPosY);
+    }
+
+    private bool CheckIsBlocked(int unitPosX, int unitPosY, int targetPosX, int targetPosY)
+    {
+        var (wid, hei, startX, startY) = GetCheckRange(unitPosX, unitPosY, targetPosX, targetPosY);
+        for (int i = 0; i < hei; i++)
+        {
+            for (int j = 0; j < wid; j++)
+            {
+                if (CheckIsThrough(startX + j, startY + i, unitPosX, unitPosY, targetPosX, targetPosY) && mapLayout[(startX + j) + (width * (i + startY))])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private (int wid, int hei, int startX, int startY) GetCheckRange(int unitPosX, int unitPosY, int targetPosX, int targetPosY)
+    {
+        int wid = Math.Abs(targetPosX - unitPosX) + 1;
+        int hei = Math.Abs(targetPosY - unitPosY) + 1;
+        int startX = 0;
+        if (unitPosX > targetPosX)
+        {
+            startX = targetPosX;
+        }
+        else
+        {
+            startX = unitPosX;
+        }
+        int startY = 0;
+        if (unitPosY > targetPosY)
+        {
+            startY = targetPosY;
+        }
+        else
+        {
+            startY = unitPosY;
+        }
+        return (wid, hei, startX, startY);
+    }
+
+    private bool CheckIsThrough(int checkPosX, int checkPosY, int unitPosX, int unitPosY, int targetPosX, int targetPosY)
+    {
+        int vecX = targetPosX - unitPosX;
+        int vecY = targetPosY - unitPosY;
+        int posX = unitPosX * 2 + 1;
+        int posY = unitPosY * 2 + 1;
+        Span<int> result = stackalloc int[4];
+        result[0] = CalcCrossProduct(vecX * 2, vecY * 2, posX, posY, checkPosX * 2, checkPosY * 2);
+        result[1] = CalcCrossProduct(vecX * 2, vecY * 2, posX, posY, (checkPosX + 1) * 2, checkPosY * 2);
+        result[2] = CalcCrossProduct(vecX * 2, vecY * 2, posX, posY, checkPosX * 2, (checkPosY + 1) * 2);
+        result[3] = CalcCrossProduct(vecX * 2, vecY * 2, posX, posY, (checkPosX + 1) * 2, (checkPosY + 1) * 2);
+        int max = -1000;
+        int min = 1000;
+        if (result[0] * result[1] * result[2] * result[3] == 0)
+        {
+            return true;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if (max < result[i])
+            {
+                max = result[i];
+            }
+            if (min > result[i])
+            {
+                min = result[i];
+            }
+        }
+        if (max > 0 && min < 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private int CalcCrossProduct(int vecX, int vecY, int posX, int posY, int checkPosX, int checkPosY)
+    {
+        long result = vecX * (checkPosY - posY) - vecY * (checkPosX - posX);
+        if (result < 0)
+        {
+            return -1;
+        }
+        if (result == 0)
+        {
+            return 0;
+        }
+        if (result > 0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
+
+public enum UnitFacing
+{
+    North,
+    East,
+    South,
+    West
+}
