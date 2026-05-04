723efa76d4a3a2cfb3b7295cc7019ee6be4e6417
AlteNielsen <alte.nielsen@gmail.com>
2026-05-04 21:32:41 +0900

    追加：ユニットの消火範囲のプレビュー

--- Assets/Scenes/Stage/StageSceneView.cs
+++ Assets/Scenes/Stage/StageSceneView.cs
@@ -1,14 +1,87 @@
 using System;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StageSceneView
 {
     private UIDocument document;
+    private List<VisualElement> rangeDisplays;
+    private int[] unitIndices;
 
     public StageSceneView(UIDocument doc)
     {
         document = doc;
+        DisplayUnitRanges();
+    }
+
+    public void LightUpSelectUnitRange(int index)
+    {
+        for (int i = 0; i < rangeDisplays.Count; i++)
+        {
+            rangeDisplays[i].AddToClassList("display-tile-range-not-selected");
+        }
+
+        if(index >= 0)
+        {
+            rangeDisplays[unitIndices[index]].RemoveFromClassList("display-tile-range-not-selected");
+        }
+    }
+
+    private void DisplayUnitRanges()
+    {
+        rangeDisplays = document.rootVisualElement.Query<VisualElement>("UnitRangeDisplay").ToList();
+        ReadOnlySpan<float> unit = SaveDataManager.Instance.Access<UnitSelectChunk>((int)SaveDataManager.SaveDataChunk.UnitSelect).data.Span;
+        ReadOnlySpan<float> levels = SaveDataManager.Instance.Access<UnitLevelChunk>((int)SaveDataManager.SaveDataChunk.UnitLevel).data.Span;
+        for(int i = 0; i < rangeDisplays.Count; i++)
+        {
+            rangeDisplays[i].AddToClassList("display-tile-range-not-selected");
+        }
+
+        unitIndices = new int[unit.Length];
+        int counter = 0;
+        for (int i = 0; i < unit.Length; i++)
+        {
+            if (unit[i] < 0.5f) continue;
+
+            unitIndices[i] = counter;
+            int size = CulculateLibrary.GetUnitRangeDisplaySize(i, (int)levels[i] + 1);
+            var(centerX, centerY) = CulculateLibrary.GetUnitRangeCenterPos(i, (int)levels[i] + 1);
+            for (int j = 0; j < size * size; j++)
+            {
+                VisualElement ve = new();
+                ve.style.width = Length.Percent(100f / size);
+                ve.style.height = Length.Percent(100f / size);
+                if(IsTheTileUnitRange(i, (int)levels[i] + 1, j, centerX, centerY, size))
+                {
+                    ve.AddToClassList("display-tile-range");
+                }
+                if(j == centerY * size + centerX)
+                {
+                    ve.AddToClassList("display-tile-unit");
+                }
+                rangeDisplays[counter].contentContainer.Add(ve);
+            }
+            counter++;
+        }
+    }
+
+    private bool IsTheTileUnitRange(int index, int level, int target, int centerX, int centerY, int size)
+    {
+        Span<RangeData> data = UnitDataBase.Datas[index].RangeData;
+        for(int i = 0; i < level; i++)
+        {
+            for(int j = 0; j < data[i].range.Length; j++)
+            {
+                int x = data[i].range[j].relativeX + centerX;
+                int y = data[i].range[j].relativeY + centerY;
+                if(y * size + x == target)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
