6177aa13f8b0f977a522708bbbd17584abbec131
AlteNielsen <alte.nielsen@gmail.com>
2026-05-15 00:04:09 +0900

    Fixes #54 #55

--- Assets/DataBase/TextDataBase.cs
+++ Assets/DataBase/TextDataBase.cs
@@ -58,6 +58,7 @@ public static class TextDataBase
         EndingScreen,
         EndingView,
         StageLoading,
-        Stage
+        Stage,
+        Clear
     }
 }
--- Assets/Scenes/Clear/ClearSceneManager.cs
+++ Assets/Scenes/Clear/ClearSceneManager.cs
@@ -1,11 +1,17 @@
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ClearSceneManager : MonoBehaviour
 {
+    [SerializeField] private UIDocument document;
     async void Awake()
     {
+        ExtinguishingContract.DevelopOnlyGameSetup();
+        document.rootVisualElement.Q<Label>("Text").text = TextDataBase.GetTexts(TextDataBase.TextDictionary.Clear)[0];
+        VisualElement fader = document.rootVisualElement.Q<VisualElement>("Fader");
+        CulculateLibrary.SceneFadeIn(fader);
         await Task.Delay(3000);
-        GameSceneManager.ToTitle();
+        CulculateLibrary.SceneFadeOut(fader, GameSceneManager.ToTitle);
     }
 }
